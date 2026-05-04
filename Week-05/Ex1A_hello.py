7bda37e0fdcd0d2831588e4bbabb404970fb9017
Naomi Tewolde <naomitewolde9@gmail.com>
2026-05-04 11:18:37 -0400

    "Updated Ex1A Python VScode"

--- Week-05/Ex1A_hello.py
+++ Week-05/Ex1A_hello.py
@@ -1,1 +1,23 @@
 print("Hello World")
+
+message = 'Hello World'
+print(message)
+
+# Hello World prints twice because these are two different ways to get the same output. 
+# The second way we did this was creating a varible for the value "message" and then using 
+# a simple print () function 
+
+
+# Displaying dollars and cents
+
+dollars =  3
+cents = .50
+print(dollars + cents) 
+
+cents = cents + .25
+
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + " " + c_str) 
