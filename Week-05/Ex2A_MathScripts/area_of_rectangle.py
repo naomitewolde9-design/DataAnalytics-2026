989084a7ec637dad4cf5543636eaf732fa6ad853
Naomi Tewolde <naomitewolde9@gmail.com>
2026-05-04 23:35:29 -0400

    "Ex2B Net Worth"

---
+++ Week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,8 @@
+a = 2
+b = 13 
+
+# print(a*b) 
+
+print('Side A is: ' + str(a))
+print('Side B is: ' + str(b))
+print('The area of the rectangle is: ' + str(a*b))
---
+++ Week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,7 @@
+total_assets = 100000
+debts = 2000
+net_worth = 98000
+
+print('Your total assets are:' + " " + str(total_assets))
+print('Your total debts are:' + " " + str(debts))
+print('Your net worth is:' + " " + str(net_worth))
