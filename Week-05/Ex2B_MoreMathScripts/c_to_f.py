fcd382f96771243b50a3d02444e8cbd666f07022
Naomi Tewolde <naomitewolde9@gmail.com>
2026-05-06 14:09:07 -0400

    "c to f using f-string"

---
+++ Week-05/Ex2B_MoreMathScripts/c_to_f.py
@@ -0,0 +1,10 @@
+celsius = 33.2
+fahrenheit = (celsius * 9/5) + 32 
+
+print(fahrenheit)
+
+#f-string 
+
+print(f'If celsius is {celsius} C then fahrenheit would be {fahrenheit}F')
+#91.76
+#If celsius is 33.2 C then fahrenheit would be 91.76F
