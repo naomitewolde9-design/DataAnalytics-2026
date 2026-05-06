6ffeb2c54585ba7fddcf6ec68fea57cd0fbf9cb5
Naomi Tewolde <naomitewolde9@gmail.com>
2026-05-06 14:10:30 -0400

    "c to f updated using f-string"

--- Week-05/Ex2B_MoreMathScripts/c_to_f.py
+++ Week-05/Ex2B_MoreMathScripts/c_to_f.py
@@ -5,6 +5,6 @@ print(fahrenheit)
 
 #f-string 
 
-print(f'If celsius is {celsius} C then fahrenheit would be {fahrenheit}F')
+print(f'If celsius is {celsius} C then fahrenheit would be {fahrenheit} F')
 #91.76
 #If celsius is 33.2 C then fahrenheit would be 91.76F
