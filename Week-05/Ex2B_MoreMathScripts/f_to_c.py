4c56272a70ba752b8a2e03150d3e988bbb1038d8
Naomi Tewolde <naomitewolde9@gmail.com>
2026-05-06 12:26:00 -0400

    "Fahrenheit to Celsius"

---
+++ Week-05/Ex2B_MoreMathScripts/f_to_c.py
@@ -0,0 +1,6 @@
+#Coverting a temperature from Fahrenheit to Celsius
+fahrenheit = 77
+celsius = (fahrenheit - 32) * 5/9 # formula for farenheit to celsius is sub 32 and multiply 5/9 (had to look this up)
+
+print(celsius)
+#25.0
