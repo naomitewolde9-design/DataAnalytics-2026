01197896df98a7a24168e1f4feefd30e2e899534
Naomi Tewolde <naomitewolde9@gmail.com>
2026-05-06 14:19:51 -0400

    "Taxes monthly"

---
+++ Week-05/Ex2B_MoreMathScripts/taxes.py
@@ -0,0 +1,8 @@
+x = 4000
+taxes = 0.23
+m_taxes = x * taxes
+print(m_taxes)
+
+print(f'If your monthly salary is ${x} and the taxes is 23% then the amount of taxes you pay in a month is ${m_taxes: .2f} ')
+#920.0
+#If your monthly salary is $4000 and the taxes is 23% then the amount of taxes you pay in a month is $ 920.00 
