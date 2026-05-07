20df7a143325154e875f2ffc8a3a785afabecbf7
Naomi Tewolde <naomitewolde9@gmail.com>
2026-05-07 19:06:27 -0400

    "Ex3A lab 1: Slicing, strip, int, and float"

--- Week-05/Ex3A_conversion_tests.py
+++ Week-05/Ex3A_conversion_tests.py
@@ -2,11 +2,12 @@
 # conversion techniques
 # Author: Sam Q. Newprogrammer
 
-a = " 101.1 "
-b = '55'
-c = "402 Stevens"
-d = 'Number 5 '
+a = " 101.1 " # <class 'str'>
+b = '55'      # <class 'str'>
+c = "402 Stevens"  # <class 'str'>
+d = 'Number 5 '    # <class 'str'>
 
+#Value types: 
 print(a)
 print(type(a))
 print()
@@ -18,4 +19,48 @@ print(type(c))
 print()
 print(d)
 print(type(d))
+print()
 
+
+# Fixing a with error: (a)
+
+af = float(a) # works
+anf = int(float(a)) # works
+
+slic_a = a[1:6] # works 101.1 without any spacing (used slicing)
+print(float(slic_a)) 
+print(slic_a)
+
+# a_int = int(a) ValueError: invalid literal for int() with base 10: ' 101.1 '
+print(a.strip()) #101.1, removing the leading and trailing spaces 
+
+
+# Fixing with errors: (b)
+
+bf = float(b) # works
+bif = int(float(b)) # works
+
+slic_b = b[0:3] # works
+print(int(slic_b)) # works 
+print(slic_b) # works 55, without the qoutations 
+
+
+# fixing with errors: (c)
+
+# cf = float(c) ValueError: could not convert string to float: '402 Stevens'
+# cif = int(float(c)) ValueError: could not convert string to float: '402 Stevens'
+
+slic_c = c[0:4] #works
+print(int(slic_c)) # works
+print(slic_c) # works, 402 
+
+
+# Fixing with error: (d)
+
+# df = float(d) ValueError: could not convert string to float: 'Number 5 '
+# dif = int(float(d)) ValueError: could not convert string to float: 'Number 5 '
+
+slic_d = d[7:9] # works
+print(slic_d) # works, 5
+print(int(slic_d)) # works
+print(d.strip()) # work, Number 5
