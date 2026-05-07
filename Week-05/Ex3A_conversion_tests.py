d42e713e16404b48aa2f8e9e71254ec081eb6029
Naomi Tewolde <naomitewolde9@gmail.com>
2026-05-07 15:40:13 -0400

    "5/7"

---
+++ Week-05/Ex3A_conversion_tests.py
@@ -0,0 +1,21 @@
+# Description: This script tests various numeric 
+# conversion techniques
+# Author: Sam Q. Newprogrammer
+
+a = " 101.1 "
+b = '55'
+c = "402 Stevens"
+d = 'Number 5 '
+
+print(a)
+print(type(a))
+print()
+print(b)
+print(type(b))
+print()
+print(c)
+print(type(c))
+print()
+print(d)
+print(type(d))
+
