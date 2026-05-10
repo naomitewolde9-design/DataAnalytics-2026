d265232c7b3eb3b37a0e7f3d5e64d8db2e464162
Naomi Tewolde <naomitewolde9@gmail.com>
2026-05-10 18:20:58 -0400

    "Greetings"

---
+++ Week-05/Ex4B_MoreIfScripts/greeting.py
@@ -0,0 +1,10 @@
+hour = 14
+
+if 5 <= hour < 10:
+    print("Good Morning")
+elif 10 <= hour < 17: 
+    print("Good day!")
+elif 17 <= hour < 23: 
+    print("Good evening")
+elif hour >= 23 or hour < 5: 
+    print("What are you doing up so late??")
