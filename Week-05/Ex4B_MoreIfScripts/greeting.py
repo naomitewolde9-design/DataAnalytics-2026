2e935f9815324fca1ad9b49dbf825464ab316017
Naomi Tewolde <naomitewolde9@gmail.com>
2026-05-10 18:24:25 -0400

    "Greetings"

--- Week-05/Ex4B_MoreIfScripts/greeting.py
+++ Week-05/Ex4B_MoreIfScripts/greeting.py
@@ -1,10 +1,16 @@
-hour = 14
+hour = 14 # I put in multiple different hours for different outputs
 
 if 5 <= hour < 10:
     print("Good Morning")
+#using military time so this would be from 5am to 10am
 elif 10 <= hour < 17: 
     print("Good day!")
+#10am to 5pm
 elif 17 <= hour < 23: 
     print("Good evening")
+#5pm to 11pm
 elif hour >= 23 or hour < 5: 
     print("What are you doing up so late??")
+#11pm to 5am
+
+# Good day!
