3058a13508fec3eed41a52bc357b17a9e566f451
Naomi Tewolde <naomitewolde9@gmail.com>
2026-05-10 21:17:31 -0400

    "loops"

---
+++ Week-05/Ex4C_LoopScripts/loop_love.py
@@ -0,0 +1,13 @@
+great = ["awesome", "amazing", "super", "excellent"]
+
+for word in great:
+    print("loops are", word + "!")
+
+print("I <3 loops")
+
+#output
+#loops are awesome!
+#loops are amazing!
+#loops are super!
+#loops are excellent!
+#I <3 loops
